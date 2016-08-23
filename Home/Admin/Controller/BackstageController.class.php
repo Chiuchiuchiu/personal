d7ba73450a079023fc473734e507350f62a63cfe
chiuchiuchiu <chiuhey@126.com>
2016-08-23 18:40:00 +0800

    暂时提交代码，未完成分页效果

--- Home/Admin/Controller/BackstageController.class.php
+++ Home/Admin/Controller/BackstageController.class.php
@@ -11,10 +11,34 @@ namespace Admin\Controller;
 
 use Parent\Controller\PersonalController;
 use Think\Controller;
+use Think\Page;
 
 class BackstageController extends PersonalController
 {
-    public function HomePage(){
-        $this->display('homePage');
+    public function visitor_list(){
+        $model = D('Visitors');
+        $count = $model->count();
+        $page = new Page($count, 10);
+
+        $page -> setConfig('prev','上一页');
+        $page -> setConfig('next','下一页');
+        $show = $page->show();
+
+        $list = $model->get_visitor_list($page->firstRow, $page->listRows);
+//        var_dump($list);
+
+        $this->assign('page',$show);
+        $this->assign('list', $list);
+        $this->display();
+    }
+
+    public function detail(){
+        $id = I('get.id', 0, 'int');
+        var_dump($id, __CLASS__);
+    }
+
+    public function del(){
+        $id = I('get.id', 0, 'int');
+        var_dump($id, __CLASS__);
     }
 } 
---
+++ Home/Admin/Model/VisitorsModel.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaowenxi
+ * Date: 2016/8/23
+ * Time: 11:10
+ */
+
+namespace Admin\Model;
+
+use Think\Model\RelationModel;
+
+class VisitorsModel extends RelationModel{
+
+    public function get_visitor_list($first, $rows){
+        return M('visitors')->limit($first, $rows)->order('fdCreate')->select();
+    }
+}
